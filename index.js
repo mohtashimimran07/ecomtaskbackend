ede0121c15a0d420e9b46a72b6f9119d3c4ee13e
Mohtashim Imran <mohtashimimran07@gmail.com>
2024-10-01 18:16:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,205 @@
+const port=0;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Database Connections with Moongodb
+mongoose.connect("mongodb+srv://mohtashimimran07:1Vuck44bbad0mVN1@cluster0.hukgo.mongodb.net/e-commerce")
+
+//API Creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+    })
+
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port"+port)
+    }
+    else
+    {
+        console.log("Error :"+error)
+    }
+})
+const server = app.listen(0, () => {
+    const { port } = server.address();
+    console.log(`Server running on port ${port}`);
+});
+
+//Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname} ${Date.now()} ${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//Creating Upload EndPoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+res.json({
+    success:1,
+    image_url:`http://localhost:${port}/images/${req.file.filename}`
+})
+})
+
+//Scehma For Creating Products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required:true,
+    },
+    old_price:{
+    type: Number,
+    required:true,
+    },
+    date:{
+        type: Date,
+        default:Date.Now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+app.post('/addproduct',async (req,res) =>{
+    let products = await Product.find({});
+    let id;
+    if(products.lenght>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id=last_product.id+1;
+    }
+    else
+    {
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Save");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//Creating API For Deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success: true,
+        name:req.body.name
+    })
+
+})
+
+//Creating API for getting all products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Product Fetched");
+    res.send(products);
+})
+
+//Schema creating for user model
+const users=mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//Creating Endpoint for registering the user
+app.post('/signup', async (req,res)=>{
+    let check = await Users.findone({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false,errors:"existing user found with same email id"})
+    }
+    let cart ={};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+        
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+//creating endpoint for user login
+app.post('/login',async(req,res)=>{
+    
+})
+
+app.listen(port,(error)=>{
+if (!error) {
+    console.log("Server running on Port"+port)
+}
+else
+{
+console.log("Error"+error)
+}
+})
+
